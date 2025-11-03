9344342050b970fdc10818e17b5d2c40affbe2b2
Osvaldo Mesa <d3v.ozzy@gmail.com>
2025-11-02 20:06:03 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,21 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
+ * __dirname is a variable from Node.js that stores the absolute path for the dir that contains the file currently being run
  */
 module.exports = {
   siteMetadata: {
-    title: `My Frist Gatsby Site`,
+    title: `My First Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
